339a20b2f58ea5c3bef6b23cfcb42fbc35119287
binarybytes <31395956+binarybytes@users.noreply.github.com>
2023-03-17 14:24:25 -0400

    Create abuseIPDB_Bulk_IP_Lookup.ps1

---
+++ abuseIPDB_Bulk_IP_Lookup.ps1
@@ -0,0 +1,40 @@
+$key = ""
+$ioc = Get-Content .\ioc.txt
+foreach($ip in $ioc)
+{
+$url= "https://www.abuseipdb.com/check/"+$ip
+$header = @{
+        'Key' = $key;
+    }
+
+$uri = "https://api.abuseipdb.com/api/v2/check"
+    $body = @{
+        'ipAddress' = $ip;
+        'maxAgeInDays' = '365';
+        'verbose' = '';
+    }
+
+            $bulkLookup = Invoke-RestMethod -Method GET $uri -Header $header -Body $body -ContentType 'application/json; charset=utf-8'
+            $address = $bulkLookup.data.ipAddress
+            $ConfidenceScore = $bulkLookup.data.abuseConfidenceScore
+            $totalReports = $bulkLookup.data.totalReports
+            $isPublic = $bulkLookup.data.isPublic
+            $country = $bulkLookup.data.countryName
+            $Hostnames = $bulkLookup.data.hostnames
+            $UsageType = $bulkLookup.data.usageType
+            $ISP = $bulkLookup.data.isp
+            $lastReport = $bulkLookup.data.lastReportedAt
+
+             Write-Host '----------------------------------------------------------------------------'
+             Write-Host 'Confidence of abuse:' $ConfidenceScore
+             Write-Host 'Total Reports:' $totalReports
+             Write-Host 'Public:' $isPublic
+             Write-Host 'Country:' $country
+             Write-Host 'IP:' $address
+             Write-Host 'Hostname:' $Hostnames
+             Write-Host 'Usage Type:' $UsageType
+             Write-Host 'ISP:' $ISP
+             Write-Host 'Last reported:' $lastReport
+             Write-Host '----------------------------------------------------------------------------'
+
+        }
